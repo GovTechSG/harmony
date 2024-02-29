4e44ce237777566e5836d53c7b25d40ac20569d2
Aniruddha Adhikary <aniruddha_adhikary@tech.gov.sg>
2024-02-29 21:01:23 +0800

    chore: add batch processing

--- demo.py
+++ demo.py
@@ -2,9 +2,11 @@ import json
 import os
 from typing import Tuple
 
+import pandas as pd
 import streamlit as st
 from networkx import DiGraph
 from openai import OpenAI
+from streamlit.runtime.uploaded_file_manager import UploadedFile
 
 from harmony.traversal import load_pickle
 
@@ -46,7 +48,7 @@ def get_best_candidate(choices: str, target: str) -> Tuple[str, int]:
     return message['category'], response.usage.total_tokens
 
 
-def find_product_hs_code():
+def find_product_hs_code(description: str, silent: bool = False):
     hierarchy_levels = ['Section', 'Chapter', 'Subchapter', 'Dash', 'Double Dash']
     current_node = 'root'
     total_token_use = 0
@@ -60,24 +62,30 @@ def find_product_hs_code():
 
         # If there are no successors (possible_successors is empty), we've reached the end
         if not possible_successors:
-            st.markdown(f"## Possible HS Code: {current_node}")
+            if not silent:
+                st.markdown(f"## Possible HS Code: {current_node}")
             break
 
         # Use get_best_candidate to determine the next node based on the description
         next_node, token_use = get_best_candidate(json.dumps(possible_successors), description)
         total_token_use += token_use
 
-        with st.expander(label=f"⚡️ **{level}: {next_node}** - {graph.nodes[next_node]['description']}"):
-            st.dataframe(possible_successors)
+        if not silent:
+            with st.expander(label=f"⚡️ **{level}: {next_node}** - {graph.nodes[next_node]['description']}"):
+                st.dataframe(possible_successors)
 
         # If we're at the last hierarchy level, we don't expect further successors
         if level == hierarchy_levels[-1]:
-            st.markdown(f"## Possible HS Code: {next_node}")
+            if not silent:
+                st.markdown(f"## Possible HS Code: {next_node}")
 
         # Update current_node to move down the hierarchy
         current_node = next_node
 
-    st.code(f'Tokens Used: {total_token_use}\nCost: ${0.01 / 1000 * total_token_use}')
+    if not silent:
+        st.code(f'Tokens Used: {total_token_use}\nCost: ${0.01 / 1000 * total_token_use}')
+
+    return current_node
 
 
 # UI Setup
@@ -104,12 +112,38 @@ with st.sidebar:
 st.title("📦 Harmony")
 st.markdown("Find HS Codes against a product description.")
 
-if description := st.text_input(
-        label='Product Description (type, press enter)',
-        help="Type in the textbox below and press enter/return",
-        placeholder="Xbox 360 Motherboard"):
-    try:
-        with st.spinner("🔎 Looking it up..."):
-            find_product_hs_code()
-    except KeyError:
-        st.error("The dataset appears to be corrupted for this particular section.", icon='😱')
+single_tab, batch_tab = st.tabs(["Single", "Batch Processing"])
+
+with single_tab:
+    if description := st.text_input(
+            label='Product Description (type, press enter)',
+            help="Type in the textbox below and press enter/return",
+            placeholder="Xbox 360 Motherboard"):
+        try:
+            with st.spinner("🔎 Looking it up..."):
+                find_product_hs_code(description=description, silent=False)
+        except KeyError:
+            st.error("The dataset appears to be corrupted for this particular section.", icon='😱')
+
+with batch_tab:
+    uploaded_file = st.file_uploader(label="CSV File", accept_multiple_files=False)
+    if uploaded_file:
+        df = pd.read_csv(uploaded_file)
+
+        if 'description' not in df.columns or 'hs_code' not in df.columns:
+            st.error("CSV file does not have a `description` and `hs_code` column.", icon='⚠️')
+        else:
+            df = df[['description', 'hs_code']]
+            df_placeholder = st.empty()
+            with df_placeholder.container():
+                st.dataframe(df)
+            df['matched'] = ''  # Initialize the matched column with empty strings
+            if st.button(label='Start', type='primary'):
+                with st.spinner("Processing..."):
+                    for index, row in df.iterrows():
+                        result = find_product_hs_code(description=row['description'], silent=True)
+                        df.at[index, 'result'] = result
+                        df.at[index, 'matched'] = 'pass' if row['hs_code'] == result else 'fail'
+                        with df_placeholder.container():
+                            st.dataframe(df)
+                st.success("Processing complete!")
