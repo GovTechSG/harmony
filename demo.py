61cebff13d3d2605fda4658828413916c3510414
Aniruddha Adhikary <aniruddha_adhikary@tech.gov.sg>
2024-02-29 08:53:06 +0800

    chore: make the demo app dynamic

--- demo.py
+++ demo.py
@@ -1,4 +1,5 @@
 import json
+import os
 from typing import Tuple
 
 import streamlit as st
@@ -12,7 +13,16 @@ if 'graph' not in globals():
 
 
 def get_best_candidate(choices: str, target: str) -> Tuple[str, int]:
-    client = OpenAI()
+    try:
+        assert st.session_state.get('openai_api_key')
+        client = OpenAI(
+            base_url=st.session_state['openai_base_url'],
+            api_key=st.session_state['openai_api_key']
+        )
+    except (KeyError, AssertionError) as e:
+        st.error("Please setup the API Key and/or Base URL.")
+        raise e
+
     response = client.chat.completions.create(
         model='gpt-4-1106-preview',
         messages=[
@@ -69,16 +79,37 @@ def find_product_hs_code():
 
     st.code(f'Tokens Used: {total_token_use}\nCost: ${0.01 / 1000 * total_token_use}')
 
+
 # UI Setup
 st.set_page_config(
     page_title='Harmony',
     page_icon='📦',
+    initial_sidebar_state='expanded'
 )
 
-st.title("📦 Harmony")
+with st.sidebar:
+    st.markdown("""
+    Please paste your credentials before proceeding. You may obtain your own API Key directly from
+    [OpenAI](https://platform.openai.com/api-keys).
+    """.strip())
 
-if description := st.text_input(label="Product Description", placeholder="Xbox 360 Motherboard"):
+    st.session_state['openai_api_key'] = st.text_input(label='OpenAI* API Key', type='password',
+                                                       value=os.environ.get('OPENAI_API_KEY', ''))
+
+    st.session_state['openai_base_url'] = st.text_input(label='OpenAI* Base URL',
+                                                        value=os.environ.get('OPENAI_BASE_URL',
+                                                                             'https://api.openai.com/v1'))
+    st.markdown('> `*` OpenAI Compatible')
+
+st.title("📦 Harmony")
+st.markdown("Find HS Codes against a product description.")
+
+if description := st.text_input(
+        label='Product Description (type, press enter)',
+        help="Type in the textbox below and press enter/return",
+        placeholder="Xbox 360 Motherboard"):
     try:
-        find_product_hs_code()
+        with st.spinner("🔎 Looking it up..."):
+            find_product_hs_code()
     except KeyError:
         st.error("The dataset appears to be corrupted for this particular section.", icon='😱')
